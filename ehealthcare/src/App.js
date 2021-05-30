72d31e36ec5937d49e40b8074a08fb750022938c
Dheeraj Kumar <dheeraj.s.roy@gmail.com>
2021-05-30 19:00:31 +0530

    Initial

---
+++ ehealthcare/src/App.js
@@ -0,0 +1,19 @@
+
+import './App.css';
+import { Fragment } from 'react';
+import Header from './shared/header/Header';
+
+
+import 'primereact/resources/themes/saga-blue/theme.css';
+import 'primereact/resources/primereact.min.css';
+import 'primeicons/primeicons.css';
+
+function App() {
+  return (
+    <div>
+      <Header></Header>
+    </div>
+  );
+}
+
+export default App;
---
+++ ehealthcare/src/shared/footer/Footer.js
@@ -0,0 +1,19 @@
+
+import './Footer.css';
+import { Button } from 'primereact/button';
+
+function Footer() {
+  return (
+    <div>
+<footer className="page-footer font-small blue">
+
+    <div className="footer-copyright text-center py-3">© 2020 Copyright: E-Healthcare
+       
+    </div>
+
+</footer>
+    </div>
+  );
+}
+
+export default Footer;
